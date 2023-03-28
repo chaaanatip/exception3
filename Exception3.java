55cc39461ca0b207add12c031d6120d155c78abe
chaaanatip <chanatip@Chanatips-MacBook-Air.local>
2023-03-28 14:58:09 +0700

    Exception part 1

---
+++ Exception3.java
@@ -0,0 +1,10 @@
+package exception3;
+
+import java.util.Scanner;
+
+public class Exception3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Hello World");
+    }
+}
